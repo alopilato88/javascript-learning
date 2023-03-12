ae85d52e0ee0935bbce750fdf4be478af7e626b7
alopilato88 <alex.lopilato@gmail.com>
2023-03-12 10:59:43 -0400

    Begin chapter 2 exercises.

--- 02-examples.js
+++ 02-examples.js
@@ -1,3 +1,15 @@
 console.log("Hello, world!")
 
 // Run node 02-examples.js in console to render file
+
+// Defining a float point object a_number
+const a_number = 123.45
+console.log('the type of', a_number, 'is', typeof a_number)
+
+// JavaScript does not differentiate between FP and integer 
+const a_integer = 123
+console.log("the type of", a_integer, "is", typeof a_integer)
+
+// Defining a string variable: a_string
+const a_string = "This is a string"
+console.log(a_string)
